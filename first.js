288c73aa96949bc197d65e4cd5c19fdbf97a24da
bori <soeunpark0806@gmail.com>
2025-09-15 12:55:44 +0900

    firstcommit

---
+++ first.js
@@ -0,0 +1,15 @@
+// 과제 1: 나만의 프로필 카드 만들기
+// 미션
+// 콘솔에 나만의 프로필을 출력하는 프로그램을 만들기
+// 요구사항
+//  본인의 기본 정보를 담는 변수 3개 이상 만들기
+//  number, string, boolean 타입 모두 사용하기
+//  정보를 보기 좋게 콘솔에 출력하기
+//  typeof로 변수 타입 확인해서 출력하기
+let line = '<============>'
+let name = '박소은';
+let age = '21';
+let univ = '삼육보건대 의료정보과'
+// console.log("name,/n ,age,/n,univ") 오류버전 1
+// console.log("$name\n$age$\n$univ") 오류버전 2
+console.log(`${line}\n${name}\n${age}\n${univ}\n${line}`);
